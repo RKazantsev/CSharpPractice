7cde5020bb99dbdce8d2d775253db768141e0eaf
Ruslan Kazantsev <ruslan.kazantsev@gmail.com>
2017-05-12 22:25:33 +0300

    05/12/2017 - 2

--- CSharpBook/ConsoleApp1/ConsoleApp1/Program.cs
+++ CSharpBook/ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ConsoleApp1
 {
@@ -10,7 +11,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ok");
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*************************************");
+            Console.WriteLine("***** Welcome to My Rocking App *****");
+            Console.WriteLine("*************************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ReadLine();
+            MessageBox.Show("All done!");
         }
     }
 }
---
+++ CSharpW3Resource/Basic/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            Console.Write("\nInput the First Number : ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the Second Number : ");
+            number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nThe sum of two numbers is " + (number1 + number2));
+            Console.WriteLine();
+        }
+    }
+}
